cbf376cf7162f1af55b97ccc741e54b0fea23dc6
LukasHau <lukas.hauser@hak-kitz.ac.at>
2023-02-06 16:43:15 +0100

    Kalender

--- Stundenplaner_Mader_Hauser/Calendar.cs
+++ Stundenplaner_Mader_Hauser/Calendar.cs
@@ -17,9 +17,84 @@ namespace Stundenplaner_Mader_Hauser
             InitializeComponent();
         }
 
+        int daysMultiplyer = 0;
+
         private void Calendar_Load(object sender, EventArgs e)
         {
+            lbl_name.Text = Login.username;
+            displayDays();
 
+            //select class name
+            foreach (int x in ClassSQL.LoadClassNamesID())
+            {
+                x.ToString();
+                ClassSQL.SelectClassName(x);
+
+                cB_class.Items.Add(x + ".    " + ClassSQL.NameSelectClass);
+            }
+
+        }
+
+        private void displayDays()
+        {
+            var monday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + daysMultiplyer + (int)DayOfWeek.Monday);
+            var tuesday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + daysMultiplyer + (int)DayOfWeek.Tuesday);
+            var wednesday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + daysMultiplyer + (int)DayOfWeek.Wednesday);
+            var thursday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + daysMultiplyer + (int)DayOfWeek.Thursday);
+            var friday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + daysMultiplyer + (int)DayOfWeek.Friday);
+
+            lbl_date1.Text = monday.ToString("dd. MMMM");
+            lbl_date2.Text = tuesday.ToString("dd. MMMM");
+            lbl_date3.Text = wednesday.ToString("dd. MMMM");
+            lbl_date4.Text = thursday.ToString("dd. MMMM");
+            lbl_date5.Text = friday.ToString("dd. MMMM");
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            //on the button back you get back into the main form
+            Main_admin temp = new Main_admin();
+            this.Close();
+            temp.Show();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_nextWeek_Click(object sender, EventArgs e)
+        {
+            daysMultiplyer = daysMultiplyer + 7;
+            displayDays();
+        }
+
+        private void btn_previousWeek_Click(object sender, EventArgs e)
+        {
+            daysMultiplyer = daysMultiplyer - 7;
+            displayDays();
+        }
+
+        private void btn_today_Click(object sender, EventArgs e)
+        {
+            daysMultiplyer = 0;
+            displayDays();
+        }
+
+        private void lbl_name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cB_class_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int ClassID = Convert.ToInt32(cB_class.Text.Substring(0, cB_class.Text.IndexOf(".")));
+            MessageBox.Show(ClassID.ToString());
         }
     }
 }
